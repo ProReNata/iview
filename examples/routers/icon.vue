c2d31e2afca7395c7c448cda75efe2f7cda2b0ae
Jordy Pereira <jordy.pereira@prorenata.se>
2019-02-15 17:11:46 +0100

    Build version

--- examples/routers/icon.vue
+++ examples/routers/icon.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
     <h1>Icons</h1>
+    <p>
+      Check <a href="https://fontawesome.com/icons">
+        Fontawesome
+      </a> for all the icon names
+    </p>
+    <p class="text-muted">
+      @PARAM {string} weight - default: regular - The weight of the icon. Available weights: regular, solid or light
+    </p>
+    <p class="text-muted">
+      @PARAM {string} type - required - Name of the icon
+    </p>
+    <p class="text-muted">
+      @PARAM {boolean} fw - default: false - Full width icon if true
+    </p>
     <h2>Regular</h2>
     <icon
       type="times"
       fw
+    >
+    </icon>
+    <icon
+      type="arrow-left"
+    ></icon>
+    <icon
+      type="chevron-down"
     >
     </icon>
     <h2>Solid</h2>
@@ -15,6 +36,16 @@
     <icon
       type="chevron-down"
       weight="solid"
+    >
+    </icon>
+    <h2>Light</h2>
+    <icon
+      type="arrow-left"
+      weight="light"
+    ></icon>
+    <icon
+      type="chevron-down"
+      weight="light"
     >
     </icon>
   </div>
