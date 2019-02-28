e2f04e741b87c71d4325bb4655e1978d1981d002
Jordy Pereira <jordy.pereira@prorenata.se>
2019-02-28 16:09:57 +0100

    :construction: Imported tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,11 @@ const postcssHTML = require('postcss-html');
 const postcssURL = require('postcss-url');
 const postcssSafe = require('postcss-safe-parser');
 const postcssAutoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss')('./tailwind.js');
 // const postcssNano = require('cssnano');
 // const utils = require('./DevTools/Build/utils');
 
-const plugins = [postcssHTML, postcssURL, postcssSafe, postcssAutoprefixer];
+const plugins = [postcssHTML, postcssURL, postcssSafe, tailwindcss, postcssAutoprefixer];
 
 // if (utils.IS_PRODUCTION) {
 //   plugins.push(postcssNano);
---
+++ tailwind.js
@@ -0,0 +1,374 @@
+let colors = {
+  //PRIMARY COLORS
+  p100: '#fee8bb',
+  p300: '#fcc960',
+  p500: '#fab21c', // FABRIC
+  p700: '#f9a214',
+  p900: '#f68a09',
+
+  //ACCENT COLORS
+  a100: '#ccb3b8',
+  a300: '#884d58',
+  a500: '#550011', // SOL
+  a700: '#44000c',
+  a900: '#2a0005',
+
+  //SURFACE COLORS
+  s0: '#ffffff',
+  s100: '#eff2f3',
+  s200: '#e5ebee',
+  s300: '#d0d8da',
+  s400: '#96a9ad',
+  s500: '#859ba0',
+  s600: '#748d93',
+  s700: '#657d82',
+  s800: '#586c71',
+  s900: '#4b5c60',
+
+  //TYPOGRAPHY COLORS
+  dark100: 'rgb(36, 28, 21)',
+  dark75: 'rgba(36, 28, 21, 0.75)',
+  dark50: 'rgba(36, 28, 21, 0.5)',
+  dark30: 'rgba(36, 28, 21, 0.3)',
+
+  light100: 'rgb(255, 255, 255)',
+  light75: 'rgba(255, 255, 255, 0.75)',
+  light50: 'rgba(255, 255, 255, 0.5)',
+  light30: 'rgba(255, 255, 255, 0.3)',
+
+  //MEANINGFUL COLORS
+
+  //WARNING
+  warning400: 'p100',
+  warning500: 'p300',
+  warning600: '#d6621e',
+
+  //DANGER
+  danger400: '#fbcfbd',
+  danger500: '#f7a88c',
+  danger600: '#eb5938',
+
+  //INFORMATION
+  info400: '#c5dbf2',
+  info500: '#98bce5',
+  info600: '#4576c2',
+
+  //SUCCESS
+  success400: '#d8eacc',
+  success500: '#b6d6a3',
+  success600: '#5c9642',
+
+  //UPDATE
+  update400: '#f9e0fa',
+  update500: '#f3c4f5',
+  update600: '#9a008f',
+};
+
+module.exports = {
+
+  colors: colors,
+
+  screens: {
+    'sm': '576px',
+    'md': '768px',
+    'lg': '992px',
+    'xl': '1200px',
+  },
+
+  fonts: {},
+
+  textSizes: {
+    'xs': '.75rem',     // 12px
+    'sm': '.875rem',    // 14px
+    'base': '1rem',     // 16px
+    'lg': '1.125rem',   // 18px
+    'xl': '1.25rem',    // 20px
+    '2xl': '1.5rem',    // 24px
+    '3xl': '1.875rem',  // 30px
+    '4xl': '2.25rem',   // 36px
+    '5xl': '3rem',      // 48px
+  },
+
+  fontWeights: {
+    'hairline': 100,
+    'thin': 200,
+    'light': 300,
+    'normal': 400,
+    'medium': 500,
+    'semibold': 600,
+    'bold': 700,
+    'extrabold': 800,
+    'black': 900,
+  },
+
+  leading: {
+    'none': 1,
+    'tight': 1.25,
+    'normal': 1.5,
+    'loose': 2,
+  },
+
+  tracking: {
+    'tight': '-0.05em',
+    'normal': '0',
+    'wide': '0.05em',
+  },
+
+  textColors: colors,
+
+  backgroundColors: colors,
+
+  backgroundSize: {
+    'auto': 'auto',
+    'cover': 'cover',
+    'contain': 'contain',
+  },
+
+  borderWidths: {
+    default: '1px',
+    '0': '0',
+    '2': '2px',
+    '4': '4px',
+    '8': '8px',
+  },
+
+  borderColors: global.Object.assign({ default: colors['s200'] }, colors),
+
+  borderRadius: {
+    'none': '0',
+    'sm': '.125rem',
+    default: '.25rem',
+    'lg': '.5rem',
+    'full': '9999px',
+  },
+
+  width: {
+    'auto': 'auto',
+    'px': '1px',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '24': '6rem',
+    '32': '8rem',
+    '48': '12rem',
+    '64': '16rem',
+    '1/2': '50%',
+    '1/3': '33.33333%',
+    '2/3': '66.66667%',
+    '1/4': '25%',
+    '3/4': '75%',
+    '1/5': '20%',
+    '2/5': '40%',
+    '3/5': '60%',
+    '4/5': '80%',
+    '1/6': '16.66667%',
+    '5/6': '83.33333%',
+    'full': '100%',
+    'screen': '100vw',
+  },
+
+  height: {
+    'auto': 'auto',
+    'px': '1px',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '24': '6rem',
+    '32': '8rem',
+    '48': '12rem',
+    '64': '16rem',
+    'full': '100%',
+    'screen': '100vh',
+  },
+
+  minWidth: {
+    '0': '0',
+    'full': '100%',
+  },
+
+  minHeight: {
+    '0': '0',
+    'full': '100%',
+    'screen': '100vh',
+  },
+
+  maxWidth: {
+    'xs': '20rem',
+    'sm': '30rem',
+    'md': '40rem',
+    'lg': '50rem',
+    'xl': '60rem',
+    '2xl': '70rem',
+    '3xl': '80rem',
+    '4xl': '90rem',
+    '5xl': '100rem',
+    'full': '100%',
+  },
+
+  maxHeight: {
+    'full': '100%',
+    'screen': '100vh',
+  },
+
+  padding: {
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+  },
+
+  margin: {
+    'auto': 'auto',
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+  },
+
+  negativeMargin: {
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+  },
+
+  shadows: {
+    default: '0 2px 12px rgba(0,0,0,0.06), 0 1px 2px rgba(0,0,0,0.06)',
+    'none': 'none',
+  },
+
+  zIndex: {
+    'auto': 'auto',
+    '0': 0,
+    '10': 10,
+    '20': 20,
+    '30': 30,
+    '40': 40,
+    '50': 50,
+  },
+
+  opacity: {
+    '0': '0',
+    '25': '.25',
+    '50': '.5',
+    '75': '.75',
+    '100': '1',
+  },
+
+  svgFill: {
+    'current': 'currentColor',
+  },
+
+  svgStroke: {
+    'current': 'currentColor',
+  },
+
+  modules: {
+    appearance: false,
+    backgroundAttachment: false,
+    backgroundColors: ['responsive', 'hover', 'focus'],
+    backgroundPosition: false,
+    backgroundRepeat: false,
+    backgroundSize: false,
+    borderCollapse: false,
+    borderColors: ['responsive', 'hover', 'focus'],
+    borderRadius: ['responsive'],
+    borderStyle: ['responsive'],
+    borderWidths: ['responsive'],
+    cursor: ['responsive'],
+    display: ['responsive'],
+    flexbox: ['responsive'],
+    float: false,
+    fonts: false,
+    fontWeights: ['responsive', 'hover', 'focus'],
+    height: ['responsive'],
+    leading: ['responsive'],
+    lists: false,
+    margin: ['responsive'],
+    maxHeight: ['responsive'],
+    maxWidth: ['responsive'],
+    minHeight: ['responsive'],
+    minWidth: ['responsive'],
+    negativeMargin: ['responsive'],
+    objectFit: false,
+    objectPosition: false,
+    opacity: false,
+    outline: false,
+    overflow: ['responsive'],
+    padding: ['responsive'],
+    pointerEvents: ['responsive'],
+    position: false,
+    resize: false,
+    shadows: ['responsive', 'hover', 'focus'],
+    svgFill: [],
+    svgStroke: [],
+    tableLayout: false,
+    textAlign: ['responsive'],
+    textColors: ['responsive', 'hover', 'focus'],
+    textSizes: ['responsive'],
+    textStyle: ['responsive', 'hover', 'focus'],
+    tracking: ['responsive'],
+    userSelect: ['responsive'],
+    verticalAlign: false,
+    visibility: false,
+    whitespace: false,
+    width: ['responsive'],
+    zIndex: ['responsive'],
+  },
+
+  plugins: [],
+
+  options: {
+    prefix: '',
+    important: false,
+    separator: ':',
+  },
+
+};
