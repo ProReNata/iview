e9bc09a0c1949b1c1eabf908a5d780fda61ad89d
Jordy Pereira <jordy.pereira@prorenata.se>
2019-02-28 16:42:06 +0100

    :construction: Generate tailwind less file

--- tailwind.js
+++ tailwind.js
@@ -1,68 +1,6 @@
-let colors = {
-  //PRIMARY COLORS
-  p100: '#fee8bb',
-  p300: '#fcc960',
-  p500: '#fab21c', // FABRIC
-  p700: '#f9a214',
-  p900: '#f68a09',
+import {color} from 'src/styles/less/colors.js';
 
-  //ACCENT COLORS
-  a100: '#ccb3b8',
-  a300: '#884d58',
-  a500: '#550011', // SOL
-  a700: '#44000c',
-  a900: '#2a0005',
-
-  //SURFACE COLORS
-  s0: '#ffffff',
-  s100: '#eff2f3',
-  s200: '#e5ebee',
-  s300: '#d0d8da',
-  s400: '#96a9ad',
-  s500: '#859ba0',
-  s600: '#748d93',
-  s700: '#657d82',
-  s800: '#586c71',
-  s900: '#4b5c60',
-
-  //TYPOGRAPHY COLORS
-  dark100: 'rgb(36, 28, 21)',
-  dark75: 'rgba(36, 28, 21, 0.75)',
-  dark50: 'rgba(36, 28, 21, 0.5)',
-  dark30: 'rgba(36, 28, 21, 0.3)',
-
-  light100: 'rgb(255, 255, 255)',
-  light75: 'rgba(255, 255, 255, 0.75)',
-  light50: 'rgba(255, 255, 255, 0.5)',
-  light30: 'rgba(255, 255, 255, 0.3)',
-
-  //MEANINGFUL COLORS
-
-  //WARNING
-  warning400: 'p100',
-  warning500: 'p300',
-  warning600: '#d6621e',
-
-  //DANGER
-  danger400: '#fbcfbd',
-  danger500: '#f7a88c',
-  danger600: '#eb5938',
-
-  //INFORMATION
-  info400: '#c5dbf2',
-  info500: '#98bce5',
-  info600: '#4576c2',
-
-  //SUCCESS
-  success400: '#d8eacc',
-  success500: '#b6d6a3',
-  success600: '#5c9642',
-
-  //UPDATE
-  update400: '#f9e0fa',
-  update500: '#f3c4f5',
-  update600: '#9a008f',
-};
+let colors = color;
 
 module.exports = {
 
@@ -244,7 +182,6 @@ module.exports = {
 
   margin: {
     'auto': 'auto',
-    'px': '1px',
     '0': '0',
     '1': '0.25rem',
     '2': '0.5rem',
