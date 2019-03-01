3361738fb3b9f2b73a4d9c986d26d1af2ae42ee7
Jordy Pereira <jordy.pereira@prorenata.se>
2019-03-01 16:09:25 +0100

    :construction: Add use case paragraph
    PRNJ-2828

---
+++ Storybook/Stories/Tag/UseCases.story.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Use cases</h1>
+    <h2>Paragraph</h2>
+    <p>
+      Hello! My name is <tag>Luke Skywalker</tag> and I am <tag variant="success">
+        42
+      </tag> years old. My father, <tag
+        variant="warning"
+        icon
+      >
+        Anakin Skywalker
+      </tag> has some serious anger management issues.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TagUseCases',
+};
+</script>
+
+<style scoped>
+</style>
--- Storybook/Stories/Tag/index.js
+++ Storybook/Stories/Tag/index.js
@@ -2,10 +2,12 @@ import {storiesOf} from '@storybook/vue';
 import Variants from './Variants.story.vue';
 import Icon from './Icon.story.vue';
 import Dismissable from './close.story.vue';
+import UseCases from './UseCases.story.vue';
 
 export default () => {
   storiesOf('Tag', module)
     .add('Variants', () => Variants)
     .add('Icons', () => Icon)
-    .add('Dismissable', () => Dismissable);
+    .add('Dismissable', () => Dismissable)
+    .add('Use Cases', () => UseCases);
 };
